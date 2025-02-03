106c7dc38055dc5956f2aba339510c0ea89d5ca0
Cookie987 <bili.cookie987@outlook.com>
2025-02-03 19:46:56 +0800

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,19 +10,44 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iNKORE.UI.WPF.Modern.Controls;
+using Page = iNKORE.UI.WPF.Modern.Controls.Page;
 
 namespace HVKLv2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public Pages.HomePage Page_Home = new Pages.HomePage();
+
+        private void Nav_Root_SelectionChanged(Nav_Root sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = sender.SelectedItem;
+            Page? page = null;
+
+            if (item == NavigationViewItem_Home)
+            {
+                page = Page_Home;
+            }
+
+
+            if (page != null)
+            {
+                Nav_Root.Header = page.Title;
+                Frame_Main.Navigate(page);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Nav_Root.SelectedItem = NavigationViewItem_Home;
         }
     }
 }
